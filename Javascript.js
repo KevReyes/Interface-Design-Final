866907b175937f3a9c941faac7e0108a5ddec958
KevReyes <122305927+KevReyes@users.noreply.github.com>
2023-04-12 13:03:59 -0700

    Add files via upload

--- Javascript.js
+++ Javascript.js
@@ -11,3 +11,75 @@ document.getElementById("drinks").src = arrMyPix[thePic];
 setTimeout(rotate, 2 * 1000);
 return false;
 }
+
+function processFrom(){
+
+}
+
+window.onload = rotate;
+var thePic = 0;
+var arrMyPix = new Array("ad.png", "ad2.png");
+
+function rotate(){
+    thePic++;
+    if (thePic == arrMyPix.length) {
+        thePic = 0;
+    }
+document.getElementById("ads").src = arrMyPix[thePic];
+setTimeout(rotate, 2 * 1000);
+return false;
+}
+
+function processFrom(){
+
+}
+
+const popupOverlay = document.querySelector(".popup-overlay");
+const skipButton = document.querySelector(".popup-container .skip-button");
+const visitButton = document.querySelector(".popup-container .visit-button");
+
+let remainingTime = 5;
+let allowedToSkip = false;
+let popupTimer;
+
+const createPopupCookie = () => {
+ let expiresDays = 30;
+ let date = new Date();
+ date.setTime(date.getTime() + expiresDays * 24 * 60 * 60 * 1000);
+ let expires = "expires=" + date.toUTCString();
+ document.cookie = `popupCookie=true; ${expires}; path=/;`; };
+
+const showAd = () => {
+  popupOverlay.classList.add("active");
+  popupTimer = setInterval(() => {
+    skipButton.innerHTML = `Skip in ${remainingTime}s`;
+    remainingTime--;
+
+    if (remainingTime < 0) {
+      allowedToSkip = true;
+      skipButton.innerHTML = "Skip";
+      clearInterval(popupTimer);
+    }
+  }, 1000);
+}; 
+
+const skipAd = () => {
+  popupOverlay.classList.remove("active");
+  createPopupCookie();
+};
+
+skipButton.addEventListener("click", () => {
+  if (allowedToSkip) {
+    skipAd();
+  }
+});
+
+const startTimer = () => {
+  if (window.scrollY > 100) {
+    showAd();
+    window.removeEventListener("scroll", startTimer);
+  }
+};
+
+if (!document.cookie.match(/^(.*;)?\s*popupCookie\s*=\s*[^;]+(.*)?$/)) {
+ window.addEventListener("scroll", startTimer);}
